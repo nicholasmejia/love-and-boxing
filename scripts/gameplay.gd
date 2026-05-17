b991ac2aba29173e0118e01ed31f33d2b78bef01
Nicholas <nicholasm@alldigitalrewards.com>
2026-05-16 20:46:23 -0700

    feat: compose HUD on Gameplay scene with placeholder riddle

--- scripts/gameplay.gd
+++ scripts/gameplay.gd
@@ -1,6 +1,21 @@
 extends Control
 
+@onready var _riddle: RiddleBox = $RiddleBox
+
 func _ready() -> void:
 	Globals.last_played_tier = 1
 	Globals.last_match_outcome = Globals.MatchOutcome.WIN
-	$CenterContainer/EndButton.pressed.connect(SceneRouter.goto_match_results)
+	$EndButton.pressed.connect(SceneRouter.goto_match_results)
+	_show_placeholder_prompt()
+
+func _show_placeholder_prompt() -> void:
+	var prompt := DialoguePrompt.new()
+	prompt.body_text = "[riddle goes here]"
+	prompt.answers = [_a("wrong", Outcome.Type.WRONG), _a("neutral", Outcome.Type.NEUTRAL), _a("right", Outcome.Type.RIGHT)]
+	_riddle.display(prompt)
+
+func _a(text: String, outcome: int) -> DialogueAnswer:
+	var a := DialogueAnswer.new()
+	a.text = text
+	a.outcome = outcome
+	return a
