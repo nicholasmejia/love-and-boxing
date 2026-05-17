4c2cbc9cea48a46bf50893879540c191253aad3a
Nicholas <nicholasm@alldigitalrewards.com>
2026-05-16 20:29:02 -0700

    feat: award tier unlock on match win and surface unlock badge

--- scripts/autoload/globals.gd
+++ scripts/autoload/globals.gd
@@ -1,6 +1,10 @@
 extends Node
 
+enum MatchOutcome { WIN, LOSE, DRAW }
+
 var selected_difficulty: DifficultyConfig
+var last_match_outcome: int = MatchOutcome.DRAW
+var last_played_tier: int = 1
 
 func clear_selection() -> void:
 	selected_difficulty = null
--- scripts/gameplay.gd
+++ scripts/gameplay.gd
@@ -1,4 +1,6 @@
 extends Control
 
 func _ready() -> void:
+	Globals.last_played_tier = 1
+	Globals.last_match_outcome = Globals.MatchOutcome.WIN
 	$CenterContainer/EndButton.pressed.connect(SceneRouter.goto_match_results)
